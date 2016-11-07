1a18e21f884e64a65f4c9572e9463f7f679f28d9
jmbeach <jaredbeachdesign@gmail.com>
2016-11-07 15:02:45 -0600

    db path location err fix

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+var path = require('path');
 var sqlite3 = require('sqlite3').verbose();
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
-var db = new sqlite3.Database('ideas.db',sqlite3.OPEN_READONLY,function(err) {
+var db = new sqlite3.Database(path.join(__dirname,'ideas.db'),sqlite3.OPEN_READONLY,function(err) {
 	if (err) {
 		console.log(err);
 		return;
