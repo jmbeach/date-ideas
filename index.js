78e0399de2ef7601e502a4c2cea31a366d819b17
jmbeach <jaredbeachdesign@gmail.com>
2016-12-17 14:59:24 -0600

    adding programmatic way

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-#!/usr/bin/env node
 var path = require('path');
 var sqlite3 = require('sqlite3').verbose();
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
-var db = new sqlite3.Database(path.join(__dirname,'ideas.db'),sqlite3.OPEN_READONLY,function(err) {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	db.all("SELECT * from Idea",function(errSelect, rows) {
-		if (errSelect) {
-			console.log("Something went wrong selecting ideas from database");
-			console.log(errSelect);
-			return;
-		}
-		var idea = rows[getRandomInt(0, rows.length-1)];
-		console.log(idea.Description);
-	});
-});
+
+module.exports.randomIdea = function(callback) {
+  var db = new sqlite3.Database(path.join(__dirname,'ideas.db'),sqlite3.OPEN_READONLY,function(err) {
+  	if (err) {
+      callback(err);
+      return;
+  	}
+  	db.all("SELECT * from Idea",function(errSelect, rows) {
+  		if (errSelect) {
+        callback(errSelect);
+  			return;
+  		}
+  		var idea = rows[getRandomInt(0, rows.length-1)];
+  		console.log(idea.Description);
+      callback(null, idea);
+  	});
+  });
+}
